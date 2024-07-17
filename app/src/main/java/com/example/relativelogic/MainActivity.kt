6af0d27c20138828bd03bb5321c2939ab7aab17d
shubhamKumar <appscicseshubham22@gmail.com>
2024-07-17 14:34:21 +0530

    .

--- app/src/main/java/com/example/relativelogic/MainActivity.kt
+++ app/src/main/java/com/example/relativelogic/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.relativelogic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    val btn:Button?=null
+    val btn1:Button?=null
+    val btn2:Button?=null
+    val btn3:Button?=null
+    val Text:TextView?=null
+
+    var num= 0.0f
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn=findViewById(R.id.btnadd)
+        btn1=findViewById(R.id.btnmultiply)
+        btn2=findViewById(R.id.btndivide)
+        btn3=findViewById(R.id.btnsubtract)
+
+
+        btn.setOnClickListener{
+
+        }
+
+
+
+
     }
 }
